97e29224b1f063e03f42295ec4cee3ef17a99d12
Jonah Burke <jonah@jonahb.com>
2015-02-12 20:19:23 -0500

    Convenience methods

--- lib/akismet.rb
+++ lib/akismet.rb
@@ -5,3 +5,63 @@
 }.each do |file|
   require "akismet/#{file}"
 end
+
+# {Akismet} provides convenience methods that instantiate a {Akismet::Client}
+# and invoke the Akismet API in one call. Before calling these methods, set
+# {api_key} and {home_url}.
+#
+module Akismet
+  class << self
+
+    # The API key obtained at akismet.com. Set before calling the {Akismet}
+    # class methods.
+    # @return [String]
+    attr_accessor :api_key
+
+    # A URL that identifies the application making the request. Set before
+    # calling the {Akismet} class methods.
+    # @return [String]
+    attr_accessor :home_url
+
+    # The name of the application making the request
+    # @return [String]
+    attr_accessor :app_name
+
+    # The version of the application making the request
+    # @return [String]
+    attr_accessor :app_version
+
+    # (see Client#check)
+    def check(user_ip, user_agent, params = {})
+      with_client { |client| client.check user_ip, user_agent, params }
+    end
+
+    # (see Client#spam?)
+    def spam?(user_ip, user_agent, params = {})
+      with_client { |client| client.spam? user_ip, user_agent, params }
+    end
+
+    # (see Client#spam)
+    def spam(user_ip, user_agent, params = {})
+      with_client { |client| client.spam user_ip, user_agent, params }
+    end
+
+    # (see Client#ham)
+    def ham(user_ip, user_agent, params = {})
+      with_client { |client| client.ham user_ip, user_agent, params }
+    end
+
+    # (see Client.open)
+    def open(&block)
+      with_client(&block)
+    end
+
+    private
+
+    def with_client(&block)
+      raise "Set Akismet.api_key" unless api_key
+      raise "Set Akismet.home_url" unless home_url
+      Akismet::Client.open api_key, home_url, app_name: app_name, app_version: app_version, &block
+    end
+  end
+end
---
+++ test/akismet_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class AkismetTest < MiniTest::Unit::TestCase
+
+  def setup
+    Akismet.api_key = API_KEY
+    Akismet.home_url = 'http://example.com'
+  end
+
+  [:spam?, :check, :spam, :ham].each do |method|
+    define_method("test_#{method}_succeeds") do
+      Akismet.send method, 'ip', 'ua'
+    end
+  end
+
+  def test_check_raises_if_api_key_not_set
+    Akismet.api_key = nil
+    assert_raises(RuntimeError) do
+      Akismet.check 'ip', 'ua'
+    end
+  end
+
+  def test_check_raises_if_home_url_not_set
+    Akismet.home_url = nil
+    assert_raises(RuntimeError) do
+      Akismet.check 'ip', 'ua'
+    end
+  end
+
+  def test_open_succeeds
+    Akismet.open do |client|
+      client.check 'ip', 'ua'
+    end
+  end
+
+  def test_open_raises_without_block
+    assert_raises(RuntimeError) do
+      Akismet.open
+    end
+  end
+
+end
